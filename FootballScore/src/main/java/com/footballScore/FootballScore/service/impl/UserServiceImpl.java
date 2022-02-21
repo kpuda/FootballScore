52bfbd5e0f6a93b63b5bfd39152004fd3ee44ae9
kpuda <kpuda.contact@gmail.com>
2022-02-21 14:09:02 +0100

    Added reset,save,change password

--- FootballScore/src/main/java/com/footballScore/FootballScore/service/impl/UserServiceImpl.java
+++ FootballScore/src/main/java/com/footballScore/FootballScore/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.footballScore.FootballScore.service.impl;
 
 import com.footballScore.FootballScore.entity.Token;
-import com.footballScore.FootballScore.entity.TokenType;
 import com.footballScore.FootballScore.entity.User;
+import com.footballScore.FootballScore.enums.TokenType;
+import com.footballScore.FootballScore.enums.TokenValid;
+import com.footballScore.FootballScore.model.PasswordModel;
 import com.footballScore.FootballScore.model.UserModel;
 import com.footballScore.FootballScore.repository.TokenRepository;
 import com.footballScore.FootballScore.repository.UserRepository;
@@ -44,14 +46,13 @@ public class UserServiceImpl implements UserService {
         } else {
             user = generateUser(userModel);
             String token = UUID.randomUUID().toString();
-            Token registrationToken = new Token(user, token, TokenType.NEW_ACCOUNT_VERIFICATION);
+            Token registrationToken = new Token(user, token, TokenType.NEW_ACCOUNT_VERIFICATION, TokenValid.TOKEN_UNUSED);
             url = generateVerificationTokenUrl(generateUrl(request), registrationToken);
             userRepository.save(user);
             tokenRepository.save(registrationToken);
             // TODO sendEmailWithVerificationToken(user, url);
             log.info("Url: {}", url);
             log.info("Token: {}", token);
-
         }
         return "user_registered";
     }
@@ -65,23 +66,84 @@ public class UserServiceImpl implements UserService {
         } else {
             if (registrationToken.getExpirationDate().getTime() < new Date().getTime()) {
                 return "token_expired";
+            } else if (registrationToken.getTokenValid().equals(TokenValid.TOKEN_USED)) {
+                return "token_used_already";
             } else {
-                if (!registrationToken.getTokenType().equals(TokenType.NEW_ACCOUNT_VERIFICATION)){
+                if (!registrationToken.getTokenType().equals(TokenType.NEW_ACCOUNT_VERIFICATION)) {
                     return "invalid_token_type";
                 }
+
                 User user = registrationToken.getUser();
                 if (user.isEnabled()) {
                     return "user_verified_already";
                 } else {
+                    registrationToken.setTokenValid(TokenValid.TOKEN_USED);
                     user.setEnabled(true);
                     userRepository.save(user);
+                    tokenRepository.save(registrationToken);
                 }
             }
         }
         return "user_verified";
     }
 
-    private void sendEmailWithVerificationToken(User user,String url) {
+    @Override
+    public String changePasssword(PasswordModel passwordModel, HttpServletRequest request) {
+        User user = userRepository.findByEmail(passwordModel.getEmail());
+        String url;
+        if (user == null) {
+            return "no_user_found";
+        } else {
+            if (!checkIfOldPasswordIsValid(passwordModel.getPassword(), user)) {
+                return "old_password_incorrect";
+            } else {
+                user.setPassword(passwordModel.getNewPassword());
+                userRepository.save(user);
+            }
+        }
+        return "password_changed";
+    }
+
+    @Override
+    public String resetPassword(PasswordModel passwordModel, HttpServletRequest request) {
+        User user = userRepository.findByEmail(passwordModel.getEmail());
+        String url;
+        if (user == null) {
+            return "no_user_found";
+        } else {
+            String token = UUID.randomUUID().toString();
+            Token resetPasswordToken = new Token(user, token, TokenType.FORGOT_PASSWORD, TokenValid.TOKEN_UNUSED);
+            url = generateChangePasswordTokenUrl(generateUrl(request), resetPasswordToken);
+            tokenRepository.save(resetPasswordToken);
+            //TODO sendEmailWithResetPasswordToken(user,url);
+            log.info("Url: {}", url);
+            log.info("Token: {}", token);
+        }
+        return "reset_token_send";
+    }
+
+    @Override
+    public String savePassword(String token, PasswordModel passwordModel) {
+        Token changePasswordToken = tokenRepository.findByToken(token);
+        if (changePasswordToken == null) {
+            return "no_token_found";
+        } else {
+            if (changePasswordToken.getExpirationDate().getTime() < new Date().getTime()) {
+                return "token_expired";
+            } else if (changePasswordToken.getTokenValid().equals(TokenValid.TOKEN_USED)) {
+                return "token_used_already";
+            } else {
+                User user = userRepository.findByEmail(changePasswordToken.getUser().getEmail());
+                changePasswordToken.setTokenValid(TokenValid.TOKEN_USED);
+                user.setPassword(passwordEncoder.encode(passwordModel.getNewPassword()));
+                userRepository.save(user);
+                tokenRepository.save(changePasswordToken);
+            }
+        }
+        return "password_changed";
+    }
+
+    private void sendEmailWithVerificationToken(User user, String url) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("noreply@baeldung.com");
         message.setTo(user.getEmail());
@@ -90,6 +152,17 @@ public class UserServiceImpl implements UserService {
         message.setReplyTo(user.getEmail());
         javaMailSender.send(message);
     }
+
+    private void sendEmailWithResetPasswordToken(User user, String url) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("football@score.com");
+        message.setTo(user.getEmail());
+        message.setSubject("Reset password");
+        message.setText(url);
+        message.setReplyTo(user.getEmail());
+        javaMailSender.send(message);
+    }
+
 
     private User generateUser(UserModel userModel) {
         User user = new User();
@@ -111,5 +184,15 @@ public class UserServiceImpl implements UserService {
         return applicationUrl +
                 "/verifyRegistration?token=" +
                 token.getToken();
+    }
+
+    private String generateChangePasswordTokenUrl(String generateUrl, Token token) {
+        return generateUrl
+                + "/savePassword?token="
+                + token.getToken();
+    }
+
+    private boolean checkIfOldPasswordIsValid(String oldPassword, User user) {
+        return passwordEncoder.matches(oldPassword, user.getPassword());
     }
 }
